c2d75bcfd533c10ea850d6067f3db6130b3ce6c7
mobileappscloud <doc92606@gmail.com>
2016-08-25 15:23:57 -0700

    implemented get request with Alamofire as well as map with long gesture recognizer

--- MyWeatherFinder/ViewController.swift
+++ MyWeatherFinder/ViewController.swift
@@ -7,9 +7,65 @@
 //
 
 import UIKit
+import MapKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var IBMapView: MKMapView!
+    
+    let OpenWeatherAPIKey = "dbcd27cf15e7a10315558c3ae018d7ef"
+    
+    func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer)
+    {
+        let touchPoint = gestureRecognizer.locationInView(IBMapView)
+        let newCoordinates = IBMapView.convertPoint(touchPoint, toCoordinateFromView: IBMapView)
+        
+        addAnnotationOnLocation(newCoordinates)
+    }
+    
+    func addAnnotationOnLocation(coordinates: CLLocationCoordinate2D)
+    {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinates
+        annotation.title = "Loading..."
+        annotation.subtitle = "Loading..."
+        IBMapView.addAnnotation(annotation)
+        
+        //Alamofire
+        
+        let url = "http://api.openweathermap.org/data/2.5/weather?lat=\(coordinates.latitude)&lon=\(coordinates.longitude)&APIKEY=\(OpenWeatherAPIKey)&units=metric"
+        
+        Alamofire.request(.GET, url).responseJSON { (response) in
+            
+            switch response.result
+            {
+            case .Success(let data):
+                dispatch_async(dispatch_get_main_queue())
+                {
+                    if let locationName:String = data["name"] as? String
+                    {
+                        annotation.title = locationName
+                    }
+                    
+                    if let main:[NSObject:AnyObject] = data["main"] as? [NSObject:AnyObject]
+                    {
+                        if let temperature = main["temp"]
+                        {
+                            annotation.subtitle = "\(temperature)℃"
+                            self.IBMapView.selectAnnotation(annotation, animated: true)
+                        }
+                    }
+                }
+                
+            case .Failure(let error):
+                print(error)
+            }
+        }
+        
+
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -20,6 +76,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func IBActionLongPress(sender: UILongPressGestureRecognizer)
+    {
+        handleLongPress(sender)
+    }
 
 }
 
